e6e6dae41845728c8df417e0c30973f3bc32a4fd
Artem Kartavtsev <arkartavtsev@gmail.com>
2023-12-13 22:08:07 +0300

    feat: Add game board

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -5,5 +5,9 @@ export const Config = {
     loader: Tools.massiveRequire(require["context"]('./../../sprites/', true, /\.(mp3|png|jpe?g)$/)),
     scenes: {
         "Game": Game
+    },
+    board: {
+        rows: 6,
+        cols: 6
     }
 };
--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -1,14 +1,28 @@
+import * as PIXI from "pixi.js";
 import { App } from "../system/App";
 import { Scene } from "../system/Scene";
+import { Board } from "./Board";
 
-export class Game extends Scene {
+export class Game {
+    constructor() {
+        this.container = new PIXI.Container();
+        
+        this.create();
+    }
+
     create() {
         this.createBackground();
+        this.createBoard();
     }
     createBackground() {
         this.bg = App.sprite("bg");
         this.bg.width = window.innerWidth;
         this.bg.height = window.innerHeight;
         this.container.addChild(this.bg);
+    }
+    createBoard() {
+        this.board = new Board();
+        
+        this.container.addChild(this.board.container);
     }
 }
